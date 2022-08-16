87c8e58b52f0c2fb50eb522a238cc40f3715bf50
NahuelUboldi <nahueluboldi@gmail.com>
2022-08-16 17:21:31 -0300

    config files

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,16 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-var __webpack_exports__ = {};
-/*!**********************!*\
+/******/ (() => {
+  // webpackBootstrap
+  /******/ 'use strict';
+  var __webpack_exports__ = {};
+  /*!**********************!*\
   !*** ./src/index.ts ***!
   \**********************/
 
-let variable = 'hola';
-console.log(variable + ' mundo');
-function woof(noise) {
+  let variable = 'hola';
+  console.log(variable + ' mundo');
+  function woof(noise) {
     console.log(noise);
-}
+  }
 
-/******/ })()
-;
+  /******/
+})();
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ let variable: string = 'hola';
 
 console.log(variable + ' mundo');
 
+const obj = {
+  one: {
+    two: {
+      three: 'awoooo',
+    },
+  },
+};
+
 function woof(noise: any) {
   console.log(noise);
 }
+
+woof(obj);
